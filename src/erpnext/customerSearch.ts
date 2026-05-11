da79d9947d2dd688b6612e9768d5184d42d5cdae
Eelke Dollee <eelke@impertio.nl>
2026-05-11 14:00:31 +0200

    feat: add customer search via bridge.fetchList

---
+++ src/erpnext/customerSearch.ts
@@ -0,0 +1,30 @@
+import { fetchList } from "../bridge";
+
+export interface CustomerSummary {
+  name: string;
+  customer_name: string;
+  customer_primary_address: string | null;
+  email_id: string | null;
+  mobile_no: string | null;
+}
+
+const FIELDS = [
+  "name",
+  "customer_name",
+  "customer_primary_address",
+  "email_id",
+  "mobile_no",
+];
+
+export async function searchCustomers(query: string): Promise<CustomerSummary[]> {
+  const raw = await fetchList<CustomerSummary>("Customer", {
+    fields: FIELDS,
+    filters: [
+      ["customer_name", "like", `%${query}%`],
+      ["disabled", "=", 0],
+    ],
+    limit_page_length: 20,
+    order_by: "customer_name asc",
+  });
+  return raw;
+}
